741c9490f590ff23665011effdd38c22abc3ff79
learntechpuzz <learntechpuzz@gmail.com>
2019-06-23 11:44:42 +0530

    cognito changes

--- deploy.bat
+++ deploy.bat
@@ -6,7 +6,14 @@ set INPUT_FILE=template.yaml
 set OUTPUT_FILE=template-output.yaml
 set STACK_NAME=%APPLICATION_NAME%-%STAGE_NAME%
 
-:: Build Lambda Functions using maven
+:: Build Cognito User Pool Client and Domain Settings Lambda Functions using npm
+
+cd cognito-user-pool-client-domain
+call npm install
+call npm prune --production
+cd ..
+
+:: Build Application Lambda Functions using maven
 
 cd lambda-functions
 call mvn clean package
@@ -18,6 +25,9 @@ call aws s3 mb s3://%S3_BUCKET%
 :: copy swagger.yaml
 call aws s3 cp swagger.yaml s3://%S3_BUCKET%/swagger.yaml
 
+:: package
 call aws cloudformation package --template-file %INPUT_FILE% --output-template-file %OUTPUT_FILE% --s3-bucket %S3_BUCKET%
-call aws cloudformation deploy --template-file %OUTPUT_FILE% --stack-name %STACK_NAME% --parameter-overrides ApplicationName=%APPLICATION_NAME% StageName=%STAGE_NAME% S3BucketName=%S3_BUCKET% --capabilities CAPABILITY_IAM
+
+:: deploy
+call aws cloudformation deploy --template-file %OUTPUT_FILE% --stack-name %STACK_NAME% --parameter-overrides ApplicationName=%APPLICATION_NAME% StageName=%STAGE_NAME% S3BucketName=%S3_BUCKET% --capabilities CAPABILITY_NAMED_IAM
 
